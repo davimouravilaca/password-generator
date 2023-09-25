f755849cfd7b6a772f2f73461f95ff47ec3b78ce
starforger93 <davimouravilaca@gmail.com>
2023-09-24 21:36:14 -0300

    backend semi funcional

--- public/passwordgeneration.php
+++ public/passwordgeneration.php
@@ -1,9 +1,48 @@
 <?php
-$PasswordLength = $_POST['PasswordLength'];// Corrija o nome da variável para corresponder ao enviado pelo JavaScript
+$PasswordLength = $_POST['PasswordLength'];
 $IncludeNumbers = $_POST['IncludeNumbers'];
 $IncludeSymbols = $_POST['IncludeSymbols'];
 
-$generatedPasswordText = md5(mt_rand(0, 999)) . "exemplo";
+$caracteres = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+$numbers = "0123456789";
 
-echo $generatedPasswordText; // Corrigido o uso do echo
+switch ($PasswordLength) {
+    case 1:
+        $PasswordLength = 4;
+        break;
+    case 2:
+        $PasswordLength = 8;
+        break;
+    case 3:
+        $PasswordLength = 12;
+        break;
+    case 4:
+        $PasswordLength = 15;
+        break;
+    case 5:
+        $PasswordLength = 20;
+        break;
+}
+
+if ($IncludeNumbers == true) {
+    $caracteres .= $numbers;
+}
+
+if ($IncludeSymbols == true) {
+    $caracteres .= "!@#$%^&*()_+-={}[]|\\:;\"'<>,.?/~`";
+}
+
+if ($PasswordLength == 4){
+    $caracteres = $numbers;
+}
+
+$password = '';
+$caracteresLength = strlen($caracteres);
+
+for ($i = 0; $i < $PasswordLength; $i++) {
+    $randomIndex = mt_rand(0, $caracteresLength - 1);
+    $password .= $caracteres[$randomIndex];
+}
+
+echo $password;
 ?>
